f255bb797921582aa8777e28f93ef50969ca0e1f
developer <developer@DESKTOP-QPCRFIA>
2019-12-07 00:27:05 -0500

    Enviando codigos a GitHub desde Netbeans 8.2

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,13 @@
+package javaapplication2;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        //Universidad Privada del Norte
+        //Instructor: Ing. César E. Aguirre R.
+        System.out.println("Hola Mundos");
+    }
+    
+}
+
+ 
